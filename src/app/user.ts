e35658981b61b8ae300b1f85e08f5c16901b1d8f
Calebbii <biicaleb316@gmail.com>
2021-10-18 01:27:00 +0300

    adds parameters to class user

---
+++ src/app/user.ts
@@ -0,0 +1,3 @@
+export class User {
+    constructor( public name:string, public login:string, public avatar_url:any, public bio:string,public followers:number, public following:number, public repoNumber:any, public html_url:any, public dateCreated: Date){}
+}
